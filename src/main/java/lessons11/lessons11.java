4b4f835c2570228da0b82e8291cecf8beb0c824e
Dmytro <dzima1233@gmail.com>
2023-05-14 21:08:36 +0300

    Home Work 11

---
+++ src/main/java/lessons11/lessons11.java
@@ -0,0 +1,22 @@
+package lessons11;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class lessons11 {
+    public static void main(String[] args) {
+        Map <Integer,String>map=new HashMap<>();
+        map.put(445,"milk");
+        map.put(334,"tea");
+        map.put(112,"cappuccino");
+        printSortMap(map);
+
+    }
+    public static void printSortMap (Map<Integer,String>map){
+        Map<Integer,String> newMap= new TreeMap<>(map);
+        for(Map.Entry<Integer,String> l:newMap.entrySet()){
+
+            System.out.println(l.getKey()+ " = "+ l.getValue());}
+    }
+}
